221858c1d0d3a90662ebdf372d82289c9c101665
OGUNLEYE OLANREWAJU EZEKIEL <103619061+larryWEB@users.noreply.github.com>
2022-10-01 04:15:30 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+/* SCROLL TO TOP FUNCTION */
+
+
+let toUP = document.querySelector(".scrollTop");
+
+window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction(){
+if (document.body.scrollTop > 1050 || document.documentElement.scrollTop > 1050){
+    toUP.style.display = "block";
+}else {
+    toUP.style.display = "none";
+}
+};
+scrollFunction();
+
+toUP.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+document.addEventListener("scroll", (e) =>{
+scrollFunction();
+})
